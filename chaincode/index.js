2454b620c76d04d84c9843bd26a0d53145e84bd7
bzp99 <bpeter@edu.bme.hu>
2026-04-19 18:04:04 +0200

    style: set up basic eslint

--- chaincode/index.js
+++ chaincode/index.js
@@ -1,9 +1,10 @@
-const { Contract } = require("fabric-contract-api");
-const crypto = require("crypto");
+import { Contract } from 'fabric-contract-api'
+import { Buffer } from 'bufer'
+import crypto from 'crypto'
 
 class KVContract extends Contract {
   constructor() {
-    super("KVContract");
+    super('KVContract')
   }
 
   async instantiate() {
@@ -11,40 +12,40 @@ class KVContract extends Contract {
   }
 
   async put(ctx, key, value) {
-    await ctx.stub.putState(key, Buffer.from(value));
-    return { success: "OK" };
+    await ctx.stub.putState(key, Buffer.from(value))
+    return { success: 'OK' }
   }
 
   async get(ctx, key) {
-    const buffer = await ctx.stub.getState(key);
-    if (!buffer || !buffer.length) return { error: "NOT_FOUND" };
-    return { success: buffer.toString() };
+    const buffer = await ctx.stub.getState(key)
+    if (!buffer || !buffer.length) return { error: 'NOT_FOUND' }
+    return { success: buffer.toString() }
   }
 
   async putPrivateMessage(ctx, collection) {
-    const transient = ctx.stub.getTransient();
-    const message = transient.get("message");
-    await ctx.stub.putPrivateData(collection, "message", message);
-    return { success: "OK" };
+    const transient = ctx.stub.getTransient()
+    const message = transient.get('message')
+    await ctx.stub.putPrivateData(collection, 'message', message)
+    return { success: 'OK' }
   }
 
   async getPrivateMessage(ctx, collection) {
-    const message = await ctx.stub.getPrivateData(collection, "message");
-    const messageString = message.toBuffer ? message.toBuffer().toString() : message.toString();
-    return { success: messageString };
+    const message = await ctx.stub.getPrivateData(collection, 'message')
+    const messageString = message.toBuffer ? message.toBuffer().toString() : message.toString()
+    return { success: messageString }
   }
 
   async verifyPrivateMessage(ctx, collection) {
-    const transient = ctx.stub.getTransient();
-    const message = transient.get("message");
-    const messageString = message.toBuffer ? message.toBuffer().toString() : message.toString();
-    const currentHash = crypto.createHash("sha256").update(messageString).digest("hex");
-    const privateDataHash = (await ctx.stub.getPrivateDataHash(collection, "message")).toString("hex");
+    const transient = ctx.stub.getTransient()
+    const message = transient.get('message')
+    const messageString = message.toBuffer ? message.toBuffer().toString() : message.toString()
+    const currentHash = crypto.createHash('sha256').update(messageString).digest('hex')
+    const privateDataHash = (await ctx.stub.getPrivateDataHash(collection, 'message')).toString('hex')
     if (privateDataHash !== currentHash) {
-      return { error: "VERIFICATION_FAILED" };
+      return { error: 'VERIFICATION_FAILED' }
     }
-    return { success: "OK" };
+    return { success: 'OK' }
   }
 }
 
-exports.contracts = [KVContract];
+export default [KVContract]
